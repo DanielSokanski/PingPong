59e81334bbe6e3591a8ae3e038a654caef8dea67
Daniel Sokanski <daniel.sokanski.programista@gmail.com>
2020-05-28 20:52:41 +0200

    speed of ball is working properly

--- Unit1.cpp
+++ Unit1.cpp
@@ -11,6 +11,9 @@ TForm1 *Form1;
 
         int x = -3;
         int y = -3;
+        int hits = 0;
+        int punkty_lewo = 0;
+        int punkty_prawo = 0;
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
@@ -20,6 +23,7 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 
 void __fastcall TForm1::Timer_pilkaTimer(TObject *Sender)
 {
+
         b->Left += x;
         b->Top += y;
 
@@ -28,17 +32,85 @@ void __fastcall TForm1::Timer_pilkaTimer(TObject *Sender)
         //Odbij od dolu
         if (b->Top+b->Height+5 >=  tlo->Height) y = -y;
         //skucie
-        if (b->Left <= p1->Left -15)
+         if(b->Left <= p1->Left -15)
              {
-        Timer_pilka->Enabled = false;
-        b->Visible=false;
-        } else if (
-                   b->Top > p1->Top - b->Height/2 &&
-                   b->Top + b->Height < p1->Top + p1->Height + b->Height/2  &&
-                   b->Left  <  p1->Left + p1->Width)
+                Timer_pilka->Enabled = false;
+                b->Visible=false;
+                nr->Visible = true;
+                ng->Visible = true;
+                Ilosc_odbic->Caption = "Ilosc odbic: " + IntToStr(hits);
+                hits = 0;
+                Ilosc_odbic->Visible = true;
+                Wynik->Caption = "Punkt dla gracza prawego >> ";
+                Wynik->Visible = true;
+                punkty_prawo++;
+                punktacja->Caption = IntToStr(punkty_lewo)+" : "+IntToStr(punkty_prawo);
+                punktacja->Visible = true;
+                }
+        if (b->Left+b->Width >= p2->Left+p2->Width +15)
+                {
+                Timer_pilka->Enabled = false;
+                b->Visible=false;
+                nr->Visible = true;
+                ng->Visible = true;
+                Ilosc_odbic->Caption = "Ilosc odbic: " + IntToStr(hits);
+                hits = 0;
+                Ilosc_odbic->Visible = true;
+                Wynik->Caption = "<< Punkt dla gracza lewego";
+                Wynik->Visible = true;
+                punkty_lewo++;
+                punktacja->Caption = IntToStr(punkty_lewo)+" : "+IntToStr(punkty_prawo);
+                punktacja->Visible = true;
+                }
+        // odbicie pi³ki
+        else if (
+                b->Top > p1->Top - b->Height/2 &&
+                b->Top + b->Height < p1->Top + p1->Height + b->Height/2  &&
+                b->Left  <  p1->Left + p1->Width)
+                {
+                if (b->Top + b->Height/2 > p1->Top + p1->Height/2-10 &&
+                    b->Top + b->Height/2 < p1->Top + p1->Height/2+10)
+                        {
+                        x = -x*2;
+
+                        hits ++;
+                        }
+                else if (x > 3 && x < -3)
+                        {
+                        x = -x/2;
+                        hits ++;
+                        }
+                else
+                        {
+                         x = -3;
+                         x = -x;
+                         hits++;
+                         }
+                }
+        else if (
+                   b->Top > p2->Top - b->Height/2 &&
+                   b->Top + b->Height < p2->Top + p2->Height + b->Height/2  &&
+                   b->Left+b->Width  >  p2->Left)
                {
-                x = -x;
+                if (b->Top + b->Height/2 > p2->Top + p2->Height/2-10 &&
+                    b->Top + b->Height/2 < p2->Top + p2->Height/2+10)
+                        {
+                        x = -x*2;
+                        hits ++;
+                        }
+                else if (x > 3 && x < -3)
+                        {
+                        x = -x/2;
+                        hits ++;
+                        }
+                else
+                        {
+                         x = 3;
+                         x = -x;
+                         hits++;
+                         }
                }
+
 
 }
 //---------------------------------------------------------------------------
@@ -57,6 +129,8 @@ void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
 {
       if (Key == 65) p1_gora->Enabled = true;
       if (Key == 90) p1_dol->Enabled = true;
+      if (Key == VK_UP) p2_gora->Enabled = true;
+      if (Key == VK_DOWN) p2_dol->Enabled = true;
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::FormKeyUp(TObject *Sender, WORD &Key,
@@ -64,5 +138,57 @@ void __fastcall TForm1::FormKeyUp(TObject *Sender, WORD &Key,
 {
         if (Key == 65) p1_gora->Enabled = false;
         if (Key == 90) p1_dol->Enabled = false;
+        if (Key == VK_UP) p2_gora->Enabled = false;
+        if (Key == VK_DOWN) p2_dol->Enabled = false;
 }
 //---------------------------------------------------------------------------
+void __fastcall TForm1::p2_dolTimer(TObject *Sender)
+{
+      if(p2->Top + p2->Height < tlo->Height - 15) p2->Top += 10;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::p2_goraTimer(TObject *Sender)
+{
+      if(p2->Top > 10) p2->Top -= 10;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::nrClick(TObject *Sender)
+{
+        b->Left = 464;
+        b->Top = 232;
+        b->Visible = true;
+        x=-3; y=-3;
+        Timer_pilka->Enabled = true;
+
+        nr->Visible = false;
+        ng->Visible = false;
+        Ilosc_odbic->Visible = false;
+        Wynik->Visible = false;
+        punktacja->Visible = false;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::ngClick(TObject *Sender)
+{
+
+        b->Left = 464;
+        b->Top = 232;
+        b->Visible = true;
+        x=-3; y=-3;
+
+
+        nr->Visible = false;
+        ng->Visible = false;
+        Ilosc_odbic->Visible = false;
+        Wynik->Visible = false;
+        punktacja->Visible = false;
+        hits = 0;
+        punkty_lewo = 0;
+        punkty_prawo = 0;
+        Timer_pilka->Enabled = true;
+        ng->Visible = false;
+}
+//---------------------------------------------------------------------------
+
--- Unit1.h
+++ Unit1.h
@@ -19,6 +19,14 @@ __published:	// IDE-managed Components
         TImage *p1;
         TTimer *p1_gora;
         TTimer *p1_dol;
+        TImage *p2;
+        TTimer *p2_gora;
+        TTimer *p2_dol;
+        TButton *nr;
+        TButton *ng;
+        TLabel *Ilosc_odbic;
+        TLabel *Wynik;
+        TLabel *punktacja;
         void __fastcall Timer_pilkaTimer(TObject *Sender);
         void __fastcall p1_goraTimer(TObject *Sender);
         void __fastcall p1_dolTimer(TObject *Sender);
@@ -26,6 +34,10 @@ __published:	// IDE-managed Components
           TShiftState Shift);
         void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
           TShiftState Shift);
+        void __fastcall p2_dolTimer(TObject *Sender);
+        void __fastcall p2_goraTimer(TObject *Sender);
+        void __fastcall nrClick(TObject *Sender);
+        void __fastcall ngClick(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
